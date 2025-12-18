2e0278d76b4ee273fe077537d12a254df535c4d2
johong1r <johongir260908@example.com>
2025-12-18 12:40:11 +0600

    'upgrade'

--- autohub/admin.py
+++ autohub/admin.py
@@ -46,15 +46,26 @@ class TransmissionAdmin(admin.ModelAdmin):
 
 @admin.register(Cars)
 class CarsAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "brand", "model", "price", "year", "body", "sale", "rent", "image_preview")
-    list_filter = ("brand", "body", "transmission", "year", "sale", "rent")
-    search_fields = ("name", "model", "color")
-    readonly_fields = ("image_preview", "join_date") 
+    list_display = (
+        "id", "name", "brand", "model",
+        "price", "year", "body",
+        "sale", "rent", "owner", "image_preview"
+    )
+
+    list_filter = (
+        "brand", "body", "transmission",
+        "year", "sale", "rent", "owner"
+    )
+
+    search_fields = ("name", "model", "color", "owner__username")
+
+    readonly_fields = ("image_preview", "join_date")
+
     list_display_links = ("id", "name")
 
     fieldsets = (
         ("Основная информация", {
-            "fields": ("name", "brand", "model", "year", "price")
+            "fields": ("name", "brand", "model", "year", "price", "owner")
         }),
         ("Характеристики", {
             "fields": ("body", "color", "seats", "transmission")
@@ -69,12 +80,20 @@ class CarsAdmin(admin.ModelAdmin):
             "fields": ("picture", "image_preview")
         }),
         ("Служебное", {
-            "fields": ("join_date",)  
+            "fields": ("join_date",)
         }),
     )
 
     def image_preview(self, obj):
         if obj.picture:
-            return mark_safe(f'<img src="{obj.picture.url}" width="120" style="border-radius:10px;">')
+            return mark_safe(
+                f'<img src="{obj.picture.url}" width="120" style="border-radius:10px;">'
+            )
         return "Нет фото"
+
     image_preview.short_description = "Превью"
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        super().save_model(request, obj, form, change)
